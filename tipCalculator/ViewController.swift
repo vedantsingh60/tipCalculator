aed8281fdf2915463b9fee93b2e69a3ae3b83012
Vedant Singh <singhva@miamioh.edu>
2020-07-23 18:15:09 +0530

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Vedant Singh on 7/23/20.
+//  Copyright © 2020 Vedant Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
